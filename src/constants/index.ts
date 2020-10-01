09edd78497859c812ff504d7325e1d9b4251a706
Baskakova Yana <bask-yana@yandex.ru>
2020-10-01 21:26:05 +0400

    change router address

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -3,7 +3,8 @@ import { AbstractConnector } from '@web3-react/abstract-connector'
 
 import { fortmatic, injected, portis, walletconnect, walletlink } from '../connectors'
 
-export const ROUTER_ADDRESS = '0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D'
+export const ROUTER_ADDRESS = '0x95eFF79aF348Ad4dB280d0898169826843284886'
+// export const ROUTER_ADDRESS = '0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D'
 
 // a list of tokens by chain
 type ChainTokenList = {
