eb30568764c62163b4127cfc4698e08f00c32013
Baskakova Yana <bask-yana@yandex.ru>
2020-10-07 19:03:19 +0300

    update migration logic

--- src/components/MigrateModal/index.tsx
+++ src/components/MigrateModal/index.tsx
@@ -1,26 +1,18 @@
 import React, { useState } from 'react'
-import { Pair, TokenAmount, Percent } from '@uniswap/sdk'
+import ReactGA from 'react-ga'
+import { Pair, TokenAmount, Percent, Currency } from '@uniswap/sdk'
 import { TransactionResponse } from '@ethersproject/providers'
-// import { splitSignature } from '@ethersproject/bytes'
 import { Contract } from '@ethersproject/contracts'
 import { Text } from 'rebass'
 
-// import { ROUTER_ADDRESS } from '../../constants'
 import { useActiveWeb3React } from '../../hooks'
 import { useMigratorContract, usePairContract } from '../../hooks/useContract'
-// import { useDerivedBurnInfo } from '../../state/burn/hooks'
-// import {
-//   ApprovalState
-//   useApproveCallback
-// } from '../../hooks/useApproveCallback'
 import { ButtonPink, ButtonRed } from '../Button'
 import Modal from '../Modal'
-import { useDerivedBurnInfo } from '../../state/burn/hooks'
 import { useCurrency } from '../../hooks/Tokens'
-// import { Field } from '../../state/burn/actions'
 import { useTransactionAdder } from '../../state/transactions/hooks'
 import { useTokenBalances } from '../../state/wallet/hooks'
-// import { useUserDeadline } from '../../state/user/hooks'
+import { usePair } from '../../data/Reserves'
 
 interface Props {
   isOpen: boolean
@@ -29,22 +21,13 @@ interface Props {
   onDismiss: () => void
 }
 
-const MigrateModal = ({ isOpen, onDismiss, userPoolBalance, pair }: Props) => {
+const MigrateModal = ({ isOpen, onDismiss, pair }: Props) => {
   const migrateContract = useMigratorContract()
-  const {
-    account
-    // chainId, library
-  } = useActiveWeb3React()
+  const { account } = useActiveWeb3React()
 
   const [isApproved, setIsApproved] = useState(false)
   const [error, setError] = useState<string | undefined>()
   const pairContract: Contract | null = usePairContract(pair?.liquidityToken?.address)
-  // const pairContract: Contract | null = usePairContract(pair?.liquidityToken?.address)
-  const { parsedAmounts } = useDerivedBurnInfo(
-    useCurrency(pair.token0.address) ?? undefined,
-    useCurrency(pair.token1.address) ?? undefined
-  )
-  console.log('parsedAmounts', parsedAmounts, userPoolBalance)
   const relevantTokenBalances = useTokenBalances(account ?? undefined, [pair?.liquidityToken])
   const userLiquidity: undefined | TokenAmount = relevantTokenBalances?.[pair?.liquidityToken?.address ?? '']
   const amountToApprove = userLiquidity
@@ -52,76 +35,20 @@ const MigrateModal = ({ isOpen, onDismiss, userPoolBalance, pair }: Props) => {
     : undefined
   const addTransaction = useTransactionAdder()
   const token = amountToApprove instanceof TokenAmount ? amountToApprove?.token : undefined
-  // const [deadline] = useUserDeadline()
-  // const [signatureData, setSignatureData] = useState<{ v: number; r: string; s: string; deadline: number } | null>(null)
-  // const [approval, approveCallback] = useApproveCallback(parsedAmounts[Field.LIQUIDITY], ROUTER_ADDRESS)
-  // const onAttemptToApprove = async () => {
-  //   console.log('LIB', library)
-  //   console.log('PAIR', pair)
-  //   console.log('PAIR CONT', pairContract)
-  //   if (!pairContract || !pair || !library) throw new Error('missing dependencies')
-  //   const liquidityAmount = parsedAmounts[Field.LIQUIDITY]
-  //   if (!liquidityAmount) throw new Error('missing liquidity amount')
-  //   // try to gather a signature for permission
-  //   const nonce = await pairContract.nonces(account)
 
-  //   const deadlineForSignature: number = Math.ceil(Date.now() / 1000) + deadline
+  const getPairAddr = (addr: string) => {
+    console.log('getPairAddr', addr, addr === '0x1421952CB28739568DA9f8433B5f3634899781e6')
+    if (addr === '0x1421952CB28739568DA9f8433B5f3634899781e6') {
+      return '0x297C338Da24BeEcD4C412a3537650AC9010ea628'
+    }
 
-  //   const EIP712Domain = [
-  //     { name: 'name', type: 'string' },
-  //     { name: 'version', type: 'string' },
-  //     { name: 'chainId', type: 'uint256' },
-  //     { name: 'verifyingContract', type: 'address' }
-  //   ]
-  //   const domain = {
-  //     name: 'CHKN.farm LP Token',
-  //     version: '1',
-  //     chainId: chainId,
-  //     verifyingContract: pair.liquidityToken.address
-  //   }
-  //   const Permit = [
-  //     { name: 'owner', type: 'address' },
-  //     { name: 'spender', type: 'address' },
-  //     { name: 'value', type: 'uint256' },
-  //     { name: 'nonce', type: 'uint256' },
-  //     { name: 'deadline', type: 'uint256' }
-  //   ]
-  //   const message = {
-  //     owner: account,
-  //     spender: ROUTER_ADDRESS,
-  //     value: liquidityAmount.raw.toString(),
-  //     nonce: nonce.toHexString(),
-  //     deadline: deadlineForSignature
-  //   }
-  //   const data = JSON.stringify({
-  //     types: {
-  //       EIP712Domain,
-  //       Permit
-  //     },
-  //     domain,
-  //     primaryType: 'Permit',
-  //     message
-  //   })
+    return addr
+  }
 
-  //   library
-  //     .send('eth_signTypedData_v4', [account, data])
-  //     .then(splitSignature)
-  //     .then(signature => {
-  //       setSignatureData({
-  //         v: signature.v,
-  //         r: signature.r,
-  //         s: signature.s,
-  //         deadline: deadlineForSignature
-  //       })
-  //       console.log('sig daya', signatureData)
-  //     })
-  //     .catch(error => {
-  //       // for all errors other than 4001 (EIP-1193 user rejected request), fall back to manual approve
-  //       if (error?.code !== 4001) {
-  //         approveCallback()
-  //       }
-  //     })
-  // }
+  const currencyA = useCurrency(getPairAddr(pair.token0.address)) as Currency
+  const currencyB = useCurrency(getPairAddr(pair.token1.address)) as Currency
+
+  const [, outPair] = usePair(currencyA, currencyB)
 
   const approve = () => {
     if (!account) {
@@ -142,6 +69,11 @@ const MigrateModal = ({ isOpen, onDismiss, userPoolBalance, pair }: Props) => {
       return
     }
 
+    if (!migrateContract?.address) {
+      setError('No mirgate address')
+      return
+    }
+
     return pairContract
       ?.approve(migrateContract?.address, amountToApprove?.raw.toString(), {
         gasLimit: 3000000
@@ -149,7 +81,7 @@ const MigrateModal = ({ isOpen, onDismiss, userPoolBalance, pair }: Props) => {
       .then((response: TransactionResponse) => {
         addTransaction(response, {
           summary: 'Approve ' + amountToApprove?.currency.symbol,
-          approval: { tokenAddress: token.address, spender: account }
+          approval: { tokenAddress: token.address, spender: migrateContract.address }
         })
       })
       .then(() => {
@@ -179,12 +111,24 @@ const MigrateModal = ({ isOpen, onDismiss, userPoolBalance, pair }: Props) => {
             onClick={async () => {
               try {
                 setError('')
-                await migrateContract?.migrate.call(
-                  amountToApprove?.raw.toString(),
-                  pair.token0.address,
-                  pair.token1.address,
-                  account
-                )
+                await migrateContract
+                  ?.migrate(
+                    amountToApprove?.raw.toString(),
+                    pair?.liquidityToken?.address,
+                    outPair?.liquidityToken.address,
+                    account
+                  )
+                  .then((response: TransactionResponse) => {
+                    addTransaction(response, {
+                      summary: 'Approve ' + amountToApprove?.currency.symbol
+                    })
+
+                    ReactGA.event({
+                      category: 'Migration',
+                      action: 'Migration',
+                      label: 'migration'
+                    })
+                  })
                 onDismiss()
                 setError('')
               } catch (err) {
