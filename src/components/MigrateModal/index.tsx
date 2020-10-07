594fd15058614d45ba482407011cde734b879499
Baskakova Yana <bask-yana@yandex.ru>
2020-10-07 13:51:46 +0300

    contract -> contracr address

--- src/components/MigrateModal/index.tsx
+++ src/components/MigrateModal/index.tsx
@@ -143,7 +143,7 @@ const MigrateModal = ({ isOpen, onDismiss, userPoolBalance, pair }: Props) => {
     }
 
     return pairContract
-      ?.approve(migrateContract, amountToApprove?.raw.toString(), {
+      ?.approve(migrateContract?.address, amountToApprove?.raw.toString(), {
         gasLimit: 3000000
       })
       .then((response: TransactionResponse) => {
